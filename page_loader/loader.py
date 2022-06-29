ca50315da281e648c1cbcd63354dd78526e88690
odhako <odhako@gmail.com>
2022-06-29 14:01:04 +0400

    Make download() function

---
+++ page_loader/loader.py
@@ -0,0 +1,16 @@
+import re
+
+import requests
+import os
+
+
+def download(directory, url):
+    r = requests.get(url)
+    filename = re.sub(r'\W', '-',
+                      re.sub(r'(^https?://)|(/$)|\.[^/]+$', '', url)
+                      ) + '.html'
+    file = os.path.join(directory, filename)
+
+    with open(file, mode='w') as html_file:
+        html_file.write(r.text)
+    print(os.path.join(directory, filename))
