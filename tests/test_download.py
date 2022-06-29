bbb4e7602a037aa096ea7c100b3b4a61d531b178
odhako <odhako@gmail.com>
2022-06-29 15:14:53 +0400

    Make test

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,8 +1,18 @@
+import tempfile
 import requests
 import requests_mock
+from page_loader import download
 
 
 def test_mock():
     with requests_mock.Mocker() as m:
         m.get('http://test.com', text='resp')
         assert requests.get('http://test.com').text == 'resp'
+
+
+def test_download():
+    with tempfile.TemporaryDirectory() as tempdir:
+        with requests_mock.Mocker() as m:
+            m.get('http://test.com', text='resp')
+            with open(download(tempdir, 'http://test.com'), 'r') as result:
+                assert result.read() == 'resp'
--- tests/test_scratch.py
+++
@@ -1,2 +0,0 @@
-def test_scratch():
-    assert 'test' == 'test'
