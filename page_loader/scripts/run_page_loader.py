e1bc3b696ecd4dd633a8dd239844164126b522c0
odhako <odhako@gmail.com>
2022-06-29 12:03:27 +0400

    Create cli script with working parameters

--- page_loader/scripts/run_page_loader.py
+++ page_loader/scripts/run_page_loader.py
@@ -1,2 +1,20 @@
+#!/usr/bin/env python
+import argparse
+
+
 def main():
-    return
+    parser = argparse.ArgumentParser(
+        description="Downloads a web page."
+    )
+    parser.add_argument('--output',
+                        help="Output directory, must be created before. "
+                             "Default is working directory.",
+                        default='default')
+    parser.add_argument('url',
+                        help='Url address with scheme, like http://')
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
