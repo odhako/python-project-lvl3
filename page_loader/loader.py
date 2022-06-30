778e0fef5ea71128914ef13c1f7dfb801deb8d77
odhako <odhako@gmail.com>
2022-06-30 12:33:19 +0400

    Add relative path support

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -4,6 +4,7 @@ import os
 
 
 def download(directory, url):
+    directory = os.path.abspath(directory)
     r = requests.get(url)
     filename = re.sub(r'\W', '-',
                       re.sub(r'(^https?://)|(/$)|html|htm', '', url)
