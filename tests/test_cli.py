37773547a07260c487a6013987e83c4b2f11595e
odhako <odhako@gmail.com>
2022-07-03 12:16:17 +0400

    Add test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,13 @@
+import pytest
 from cli_test_helpers import shell
+from page_loader.scripts.run_page_loader import main
 
 
 def test_help():
     result = shell('page-loader -h')
     assert result.exit_code == 0
+
+
+def test_error():
+    with pytest.raises(SystemExit):
+        main()
