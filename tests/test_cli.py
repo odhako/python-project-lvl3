7662c41c7fadb9f2e766d6ea0a4832c56e28ae8c
odhako <odhako@gmail.com>
2022-07-11 13:30:09 +0400

    Move non-working tests to another module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,10 +2,6 @@ import pytest
 from cli_test_helpers import shell
 from page_loader.scripts.run_page_loader import main
 
-# import tempfile
-# import requests_mock
-# import subprocess
-
 
 def test_help():
     result = shell('page-loader -h')
@@ -15,28 +11,3 @@ def test_help():
 def test_error():
     with pytest.raises(SystemExit):
         main()
-
-
-# def test_real():
-#     url = 'https://ya.ru'
-#     with tempfile.TemporaryDirectory() as tempdir:
-#         result = shell(f'page-loader --output {tempdir} {url}')
-#         assert result.stdout.endswith('ya-ru.html\n')
-
-
-# def test_with_mocking():
-#     url = 'http://test.com'
-#     with tempfile.TemporaryDirectory() as tempdir:
-#         with requests_mock.Mocker() as m:
-#             m.get(url, text='HTML')
-#             result = shell(f'page-loader --output {tempdir} {url}')
-#             assert result.exit_code == 0
-
-
-# def test_subprocess():
-#     url = 'http://test.com'
-#     with tempfile.TemporaryDirectory() as tempdir:
-#         with requests_mock.Mocker() as m:
-#             m.get(url, text='HTML')
-#             result = subprocess.run(['page-loader', '--output', tempdir, url])
-#             assert result == 0
